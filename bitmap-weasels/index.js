3fd0b83236c5cbdff457c322a4c72fe7e5bd6ef1
esack7 <isaacheist@gmail.com>
2017-08-24 14:38:24 -0700

    working on accessing pixelArray

--- bitmap-weasels/index.js
+++ bitmap-weasels/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const rf = require('./lib/bitmap');
+const tranf = require('./lib/transform');
+const Bitmap = require('./lib/bitmap');
+const fs = require('fs');
+
+let arr = [];
+
+fs.readFile(`${__dirname}/assets/palette-bitmap.bmp`, (err, data) => {
+  if(err) console.error(err)
+
+  let bmp = new Bitmap(data)
+
+  // console.log(bmp);
+  arr.push(bmp.pixelArray);
+})
