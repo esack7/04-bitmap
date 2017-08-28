66f0b52363520c4e794321249cd8f63b4eeb9782
Ian Waites <ian.waites@gmail.com>
2017-08-28 08:15:10 -0700

    starting testing

--- bitmap-weasels/__test__/bitmap.test.js
+++ bitmap-weasels/__test__/bitmap.test.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const bitmapTest = require('../lib/bitmap')
+const fs = require('fs')
+
+describe('Testing the transform methods', function() {
+
+});
--- bitmap-weasels/__test__/read-write.test.js
+++ bitmap-weasels/__test__/read-write.test.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const rwTest = require('../lib/read-write')
+const fs = require('fs')
+
+describe('Testing the transform methods', function() {
+
+});
