ee467b9381532e58af5aeefc298c7c8532cc4902
esack7 <isaacheist@gmail.com>
2017-08-27 19:06:02 -0700

    Invert and gray are created correctly now.  Need to do testing and Readme

--- bitmap-weasels/lib/read-write.js
+++ bitmap-weasels/lib/read-write.js
@@ -8,53 +8,47 @@ const fs = require('fs');
 readWrite.bmp;
 readWrite.nameKeyThing;
 
-readWrite.read = function() {
+readWrite.read = () => {
   fs.readFile(`${__dirname}/../assets/palette-bitmap.bmp`, (err, data) => {
     if(err) console.error(err);
     readWrite.bmp = new Bitmap(data);
-    console.log(readWrite.bmp);
     readWrite.black();
     fs.readFile(`${__dirname}/../assets/palette-bitmap.bmp`, (err, data) => {
       if(err) console.error(err);
       readWrite.bmp = new Bitmap(data);
-      console.log(readWrite.bmp);
       readWrite.gray();
       fs.readFile(`${__dirname}/../assets/palette-bitmap.bmp`, (err, data) => {
         if(err) console.error(err);
         readWrite.bmp = new Bitmap(data);
-        console.log(readWrite.bmp);
         readWrite.invert();
       });
     });
   });
 };
 
-readWrite.write = function(fileContent, filePath) {
+readWrite.write = (fileContent, filePath) => {
   fileContent = readWrite.bmp.allData;
   filePath = `./assets/${readWrite.nameKeyThing}.bmp`;
   fs.writeFile(filePath, fileContent, (err) => {
     if (err) throw err;
-    console.log('The file was succesfully saved!');
+    console.log(`${readWrite.nameKeyThing} has been created!`);
   });
 };
 
-readWrite.black = function(){
+readWrite.black = () => {
   readWrite.nameKeyThing = Object.keys(tranf)[2];
-  console.log('1', readWrite.nameKeyThing);
   tranf.black(readWrite.bmp);
   readWrite.write();
 };
 
-readWrite.gray = function(){
+readWrite.invert = () => {
   readWrite.nameKeyThing = Object.keys(tranf)[1];
-  console.log('2', readWrite.nameKeyThing);
-  tranf.gray(readWrite.bmp);
+  tranf.invert(readWrite.bmp);
   readWrite.write();
 };
 
-readWrite.invert = function(){
+readWrite.gray = () => {
   readWrite.nameKeyThing = Object.keys(tranf)[0];
-  console.log('1', readWrite.nameKeyThing);
-  tranf.invert(readWrite.bmp);
+  tranf.gray(readWrite.bmp);
   readWrite.write();
 };
